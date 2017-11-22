e0c9ee99165a8460c5d7357a09b8b7e2c2153ec0
Ferhat Yeşiltarla <ferhatyesiltarla@gmail.com>
2017-11-22 21:46:30 +0300

    Upt
    Update

---
+++ JS/fyStrap.js
@@ -0,0 +1,52 @@
+﻿$('.fyStrap-menu ul li').unbind('mouseover', mouseOver).unbind('mouseout', mouseOut);
+$('.fyStrap-menu ul li').bind('click', click);
+
+$('#myonoffswitch').change(function () {
+    if (this.checked) {
+        console.log("checked");
+        $('.fyStrap-menu ul li').unbind('mouseover', mouseOver).unbind('mouseout', mouseOut);
+        $('.fyStrap-menu ul li').bind('click', click);
+    }
+    else {
+        console.log("unchecked");
+        $('.fyStrap-menu ul li').unbind('click', click);
+        $('.fyStrap-menu ul li').bind('mouseover', mouseOver).bind('mouseout', mouseOut);
+    }
+});
+
+function mouseOver() {
+    var url = $('a', this).attr('data-url');
+    load(url);
+
+}
+function mouseOut() {
+    //$('.fyStrap-menu-content').hide();
+    $('.content').html("");
+    $('.loader').hide();
+}
+
+function click() {
+    var url = $('a', this).attr('data-url');
+    load(url);
+}
+
+function load(action) {
+    $.ajax({
+        type: "POST",
+        url: action,
+        dataType: "html",
+        beforeSend: function () {
+            $('.content').html("");
+            $('.loader').show();
+        },
+        success: function (result) {
+            $('.content').html(result);
+            $('.loader').hide();
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            console.log(xhr + '-' + textStatus + '-' + errorThrown);
+            alert(xhr + '-' + textStatus + '-' + errorThrown);
+            $('.loader').hide();
+        }
+    });
+}
